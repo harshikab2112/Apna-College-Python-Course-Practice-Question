098ec8844c549cdedc121d534e01f187e66fc557
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-22 10:19:22 +0530

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,6 @@
+# WAP to input 2 int numbers a and b
+# Print True if a is greater than equal to b. If not print False.
+a = int(input("Enter a num1: "))
+b = int(input("Enter a num2: "))
+
+print(a >= b)
