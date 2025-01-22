5cdcd7b6601072d0e1cda87675f91fab19ee11ad
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-22 10:23:09 +0530

    Add files via upload

---
+++ Lecture1/P2.py
@@ -0,0 +1,6 @@
+# WAP to input side of a square and print its area.
+side = float(input("Enter side od a sq.: "))
+perimeter = 4 * side
+area = side * side
+print(f"Perimeter of a sq. with side {side} is {perimeter}")
+print(f"Area of a sq. with side {side} is {area}")
