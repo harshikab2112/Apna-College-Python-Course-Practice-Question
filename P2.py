e8119956e47ac1c5df6f256e248517215b5bd42b
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-22 10:23:44 +0530

    Delete P2.py

--- P2.py
+++
@@ -1,6 +0,0 @@
-# WAP to input side of a square and print its area.
-side = float(input("Enter side od a sq.: "))
-perimeter = 4 * side
-area = side * side
-print(f"Perimeter of a sq. with side {side} is {perimeter}")
-print(f"Area of a sq. with side {side} is {area}")
