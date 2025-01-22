14e49a86caa436d1aff4cf0b2c6bce32a2c60ac6
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-22 10:23:57 +0530

    Delete P3.py

--- P3.py
+++
@@ -1,5 +0,0 @@
-# Write a Program to input 2 floating point numbers and print their average.
-a = float(input("Enter a num1: "))
-b = float(input("Enter a num2: "))
-
-print(f"Average of num1: {a} and num2: {b} is {((a+b)/2):.3f}")
