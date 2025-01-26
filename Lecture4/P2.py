d42ebb65ff5edf08a5b75dbcbf92dd5e9943645b
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-26 21:39:49 +0530

    Add files via upload

---
+++ Lecture4/P2.py
@@ -0,0 +1,20 @@
+# You are given a list of subjects for students.
+# Assume one classroom is required for 1 subject.
+# How many classrooms are needed by all students.
+# "python", "java", "C++","python","javascript",
+# "java", "python", "java", "C++", "C"
+
+mySet = {
+    "python",
+    "java",
+    "C++",
+    "python",
+    "javascript",
+    "java",
+    "python",
+    "java",
+    "C++",
+    "C"
+}
+print(mySet)
+print(len(mySet))
