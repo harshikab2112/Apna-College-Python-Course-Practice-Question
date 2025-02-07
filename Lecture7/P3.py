57f1400ac1f7e1f9c2b3ff136379fbdf42aaa5ef
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-07 21:18:08 +0530

    Add files via upload

---
+++ Lecture7/P3.py
@@ -0,0 +1,9 @@
+# Search if the word “learning” exists in the file or not.
+
+with open("practice.txt", "r") as f:
+    data = f.read()
+
+if "learning" in data:
+    print(True)
+else:
+    print(False)
